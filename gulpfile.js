c0ef4d546151ab6ae9f5bf3063e70a095f232d2f
Sam Vervaeck <sam@vervaeck.be>
2017-07-06 11:40:52 +0200

    Add a gulpfile for compiling sources and plugins

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+
+const gulp = require('gulp')
+const sourcemaps = require('gulp-sourcemaps')
+const merge = require('merge2');
+const fs = require('fs')
+const path = require('path')
+const ts = require("gulp-typescript")
+
+const projects = fs.readdirSync(path.join(__dirname, 'plugins'))
+  .map(filename => path.join(__dirname, 'plugins', filename))
+  .filter(filepath => fs.statSync(filepath).isDirectory())
+  .concat(['.'])
+
+const tsProjects = projects.map(filepath => ts.createProject(path.join(filepath, 'tsconfig.json')))
+    
+gulp.task('compile', () => {
+  return merge(projects.map((filepath, i) => 
+    gulp.src(path.join(filepath, 'src/**/*.ts'), { cwd: filepath })
+      .pipe(sourcemaps.init())
+        .pipe(tsProjects[i]())
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest(path.join(filepath, 'lib')))))
+})
+
+gulp.task('watch', ['compile'], () => {
+  gulp.watch(['src/**/*.ts', 'plugins/*/**/*.ts'], ['compile'])
+})
+
+gulp.task('default', ['compile'])
+
