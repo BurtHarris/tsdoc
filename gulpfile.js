f40126fe66e1a606861e7725059d5667cd7d4585
Burt Harris <Burt.Harris_github@azxs.33mail.com>
2017-07-06 14:12:47 -0700

    Simplify gulpfile
    Fixes: #22

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,19 @@
-
 const gulp = require('gulp')
 const sourcemaps = require('gulp-sourcemaps')
-const merge = require('merge2')
-const fs = require('fs')
-const path = require('path')
 const ts = require('gulp-typescript')
 
-const projects = fs.readdirSync(path.join(__dirname, 'plugins'))
-  .map(filename => path.join(__dirname, 'plugins', filename))
-  .filter(filepath => fs.statSync(filepath).isDirectory() && fs.existsSync(path.join(filepath, 'tsconfig.json')))
-  .concat(['.'])
-
-const tsProjects = projects.map(filepath => ts.createProject(path.join(filepath, 'tsconfig.json')))
+const tsProject = ts.createProject('tsconfig.json')
 
 gulp.task('compile', () => {
-  return merge(projects.map((filepath, i) =>
-    gulp.src(path.join(filepath, 'src/**/*.ts'), { cwd: filepath })
-      .pipe(sourcemaps.init())
-        .pipe(tsProjects[i]())
-      .pipe(sourcemaps.write())
-      .pipe(gulp.dest(path.join(filepath, 'lib')))))
+  return tsProject.src()
+    .pipe(sourcemaps.init())
+    .pipe(tsProject())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(tsProject.config.compilerOptions.outDir))
 })
 
 gulp.task('watch', ['compile'], () => {
-  gulp.watch(['src/**/*.ts', 'plugins/*/**/*.ts'], ['compile'])
+  gulp.watch(['src/**/*.ts'], ['compile'])
 })
 
 gulp.task('default', ['compile'])
