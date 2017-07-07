44eaf91d5553d71326292a06146aa48e845a9a36
Sam Vervaeck <sam@vervaeck.be>
2017-07-07 09:50:06 +0200

    Add test snippet with mixed code

---
+++ test/snippets/mixed.ts
@@ -0,0 +1,38 @@
+
+type Point = [number, number]
+
+/** 
+* Documentation for C 
+*/
+export class C { 
+  /** 
+   * constructor documentation
+   * @param a my parameter documentation
+   * @param b another parameter documentation
+   */
+  constructor(a: string, b: C) { }
+
+  foo(p: Point) {
+
+  }
+
+}
+
+/**
+ * This is just another quick test.
+ */
+export function testSome(a: number, b: number) {
+
+}
+
+export const baz: number = 3
+
+declare namespace Foo {
+  
+  export interface Baz {
+    a: string
+    b: number
+  }
+
+}
+
