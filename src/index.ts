c7db56556e97b4b91a81fab4458e0a6178cdb121
Sam Vervaeck <sam@vervaeck.be>
2017-07-07 12:31:41 +0200

    Remove unused files and imports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 
-export { TYPES } from "./constants"
 export { Renderer } from "./types"
-export { renderer, inject, injectable, injectNamed, multiInject } from "./annotations"
 export { Documenter } from "./documenter"
 
